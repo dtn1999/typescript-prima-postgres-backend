1e44b059510a776d4045a9de64ec8c4fccbc406e
danyls <danyngongang@gmail.com>
2020-11-26 13:50:56 +0100

    commit mork datat

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hello world!');
+import { main, prisma } from '../src/bootstrap/seed';
+
+main()
+  .catch((error: any) => {
+    console.error(error);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
+
+console.log('================ SCRIPT ==================');
